63ab7d23d4153d53d426cf47040bd04d90b7a368
Gummadi-Mohan-Krishna <gmohankrishnawork@gmail.com>
2025-07-12 20:42:11 +0530

    una

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
